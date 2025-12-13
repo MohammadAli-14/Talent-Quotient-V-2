a3deec10bd973aaec500168e301c18810e117eab
Muhammad5Ali <mohammad.bscs4490@iiu.edu.pk>
2025-12-13 16:17:28 +0500

    Adding frontend url

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 // middleware
 app.use(express.json());
 app.use(cors({
-  origin: ENV.CLIENT_URL || 'http://localhost:5173',
+  origin: ENV.CLIENT_URL || 'http://localhost:5173' || 'https://talent-quotient-frontend.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
